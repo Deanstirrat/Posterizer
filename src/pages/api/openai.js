8c9faf98412e948d176be5918de84975579b893a
Dean Stirrat <deanstirrat@gmail.com>
2023-03-15 13:50:21 -0700

    improved prompt quality

--- src/pages/api/openai.js
+++ src/pages/api/openai.js
@@ -15,6 +15,7 @@ export default async function handler (req, res) {
     model: "gpt-3.5-turbo",
     messages: [
       {"role": "system", content: setupPrompt},
+      {"role": "system", content: taskPrompt},
       {role: "user", content: prompt}
     ],
   });
@@ -26,4 +27,5 @@ export default async function handler (req, res) {
 }
 
 
-const setupPrompt = "You are a super intelligent AI with complex pattern recognition capabilities, a deep understanding of OCR technology and a vast knowledge of music and musical artists. I will give you a json object that is the result of running an OCR on an image displaying musical artists at an upcoming music festival. The OCR result is probably imperfect because of the stylized fashion of the festival poster. Because of this, the data you receive may have multiple artist names joined together or fragmented individual artist names into multiple segments. There may also be general festival information such as name, location, date etc. that should be removed.  Please parse the data to find the names of all musical artists. Rejoin fragmented names and separate merged names of multiple artists when necessary. Return only an array containing the found artist names and ensure it is valid json by using only double quotes. Use the data in the following message";
+const setupPrompt = "You are a super intelligent AI with complex pattern recognition capabilities, a deep understanding of OCR technology and a vast knowledge of music and musical artists.";
+const taskPrompt = "I will give you a json object that is the result of running an OCR on an image displaying musical artists at an upcoming music festival. The OCR result is imperfect because of the stylized fashion of the festival poster. Because of this, the data you receive may have the following errors: 1) Multiple artist names joined together 2) Artist names fragmented and mixed in with other names 3) Missing characters or incorrect characters in artist names 4) general festival information such as name, location, date etc. that should be removed.  Please parse the data to find the names of all musical artists and correct any errors. Rejoin fragmented names and separate merged names of multiple artists when necessary. Return only a valid json array of the found artists. return no additional commentary. Use the data in the following message:"
